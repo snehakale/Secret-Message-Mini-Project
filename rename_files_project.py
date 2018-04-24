9013c163dc9093f3ebeef90ffa9c381c047d934d
Sneha Kale <kalesneha88@gmail.com>
2018-04-24 15:21:55 -0400

    style: Format code
    Format code to change directory path.

--- rename_files_project.py
+++ rename_files_project.py
@@ -2,11 +2,11 @@ import os
 
 def rename_files():
     #getting a list of files
-    file_list = os.listdir(r"D:\Sneha Data\FSWD_Data\secret message mini project\message")
+    file_list = os.listdir(r"DIRECTORY_ADDRESS")
     print(file_list)
 
     #renaming each file
-    directory_path="D:\Sneha Data\FSWD_Data\secret message mini project\message\\"
+    directory_path="DIRECTORY_ADDRESS\\"
     for file_name in file_list:
         old_file= directory_path+file_name
         print("old file name="+old_file)
