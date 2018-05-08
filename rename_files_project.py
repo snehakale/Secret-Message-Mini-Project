fea8754fd438b99bd4b6d7e1b0c6cc175ba66489
Sneha Kale <kalesneha88@gmail.com>
2018-05-08 15:08:41 -0400

    style: Format code and add comments
    Format code and add comments in rename_files_project.py file.

--- rename_files_project.py
+++ rename_files_project.py
@@ -1,17 +1,20 @@
+# Importing os module
 import os
 
+# Function to rename files
 def rename_files():
-    #getting a list of files
+    # Getting a list of files
     file_list = os.listdir(r"DIRECTORY_ADDRESS")
     print(file_list)
 
-    #renaming each file
-    directory_path="DIRECTORY_ADDRESS\\"
+    # Renaming each file
+    directory_path = "DIRECTORY_ADDRESS\\"
     for file_name in file_list:
-        old_file= directory_path+file_name
-        print("old file name="+old_file)
-        new_file=old_file.translate(old_file.maketrans('','','0123456789'))
-        print("new file name="+new_file)
-        os.rename(old_file,new_file)
+        old_file = directory_path + file_name
+        print("old file name="+ old_file)
+        new_file = old_file.translate(old_file.maketrans('','','0123456789'))
+        print("new file name="+ new_file)
+        os.rename(old_file, new_file)
 
+# Calling a function to rename files
 rename_files()
